fe1caf2ee6f4802c4b6a0a521afd9b4965ce4853
Inanis lector <kid@kde.kiev.ua>
2023-07-08 00:33:57 +0300

    Hotfix
    Contains now uses List.Contains

--- Assets/Scripts/Unit/UnitTags.cs
+++ Assets/Scripts/Unit/UnitTags.cs
@@ -7,15 +7,7 @@ public class UnitTags : MonoBehaviour, IEnumerable
     [SerializeField] private List<UnitTypes> thisUnitTags;
 
     public bool ContainsUnitTag(UnitTypes requestedTag)
-    {
-        foreach(var unitTag in thisUnitTags)
-        {
-            if(unitTag == requestedTag)
-                return true;
-        }
-
-        return false;
-    }
+        => thisUnitTags.Contains(requestedTag);
 
     public IEnumerator GetEnumerator()
         => thisUnitTags.GetEnumerator();
