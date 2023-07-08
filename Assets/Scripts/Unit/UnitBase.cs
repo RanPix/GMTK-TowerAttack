e0f5b1449963c1d0dfb8380c3ab6b362897bd4b6
Inanis lector <kid@kde.kiev.ua>
2023-07-08 16:27:07 +0300

    Unit base Requirements

--- Assets/Scripts/Unit/UnitBase.cs
+++ Assets/Scripts/Unit/UnitBase.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(UnitMovement), typeof(UnitTags))]
 public class UnitBase : MonoBehaviour
 {
     [field: SerializeField] public UnitTemplate unitData { get; private set; }
