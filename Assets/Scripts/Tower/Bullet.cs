35b55b1f8c5dbd0b802e2f562d8fa50cce02a890
Yarik <cahloyaroslav@gmail.com>
2023-07-08 20:55:13 +0300

    Cannon was done

--- Assets/Scripts/Tower/Bullet.cs
+++ Assets/Scripts/Tower/Bullet.cs
@@ -8,24 +8,30 @@ namespace Tower
         public GameObject Target;
         [SerializeField] private Damage Damage;
         [SerializeField] private float movementSpeed;
+        [SerializeField] private Transform image;
 
         private void Update()
         {
             if (!Target)
+            {
                 Destroy(gameObject);
-            transform.LookAt(Target.transform);
+                return;
+            }
+            image.LookAt(Target.transform);
             Vector3 direction = (Target.transform.position - transform.position).normalized;
             
             transform.position += direction * movementSpeed * Time.deltaTime;
+            
         }
 
-        private void OnCollisionEnter(Collision other)
+        private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject == Target)
             {
-                Health targetHealth = Target.GetComponent<Health>();
-                targetHealth.DealDamage(Damage);
+                Target.GetComponent<UnitBase>().HP.DealDamage(Damage);
+                print(Target.GetComponent<UnitBase>().HP.Current);
             }
         }
+
     }
 }
--- Assets/Scripts/Unit/UnitBase.cs
+++ Assets/Scripts/Unit/UnitBase.cs
@@ -5,7 +5,7 @@ public class UnitBase : MonoBehaviour
 {
     [field: SerializeField] public UnitTemplate unitData { get; private set; }
     public Health HP { get; private set; }
-
+    
     private void Awake()
     {
         HP = new(this);
