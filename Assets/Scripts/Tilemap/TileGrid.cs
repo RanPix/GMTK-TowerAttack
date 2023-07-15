fb82ffacb2d2fb40d5e93970dc2bd56cffc0606d
-FantoMIStranniK- <synergyivanb@gmail.com>
2023-07-15 17:36:00 +0300

    Script cleaning
    -Deleted useless scripts
    -Tile Grid cleanded of useless methods

--- Assets/Scripts/Tilemap/TileGrid.cs
+++ Assets/Scripts/Tilemap/TileGrid.cs
@@ -1,8 +1,5 @@
-using AI;
-using JetBrains.Annotations;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 namespace TileMap
 {
@@ -10,27 +7,13 @@ namespace TileMap
     {
         public static TileGrid Instance { get; private set; }
 
-        public List<TowerTile> TowerTiles;
-
-        [SerializeField] private Tilemap tilemap;
-        [SerializeField] private TileBase[] tilemapBaseTiles;
-
-        [SerializeField] private GameObject tilePrefab;
-
-        [SerializeField] private Vector2Int gridSize;
-        [SerializeField] private Tile[,] tiles;
-        [SerializeField] private float tileSize;
-        [SerializeField] private Vector2 origin;
-        
-        [SerializeField] private List<TileType> tileTypes;
+        public List<TowerTile> TowerTiles { get; private set; } = new List<TowerTile>();
 
         [SerializeField] private List<Transform> BlankTowerTiles;
 
         private void Awake()
         {
             SetInstance();
-
-            tiles = new Tile[gridSize.x, gridSize.y];
 
             CreateTowerTiles();
         }
@@ -44,7 +27,7 @@ namespace TileMap
         }
         private void CreateTowerTiles()
         {
-            foreach(var tower in BlankTowerTiles)
+            foreach (var tower in BlankTowerTiles)
             {
                 var newTowerTile = (TowerTile)ScriptableObject.CreateInstance(typeof(TowerTile));
 
@@ -52,19 +35,6 @@ namespace TileMap
 
                 TowerTiles.Add(newTowerTile);
             }
-        }
-
-        private void Start()
-        {
-            BuildTilemap();
-        }
-
-        public Tile[,] GetTiles() => tiles;
-
-        public Tile GetTile(Vector2 tilePosition)
-        {
-            Vector2Int tileIndex = GetTileXY(tilePosition);
-            return tiles[tileIndex.x, tileIndex.y];
         }
 
         public TowerTile GetFreeTowerTile()
@@ -79,51 +49,7 @@ namespace TileMap
 
             return tile;
         }
-
-        private Vector2Int GetTileXY(Vector2 worldPosition)
-        {
-            return new Vector2Int(Mathf.FloorToInt((worldPosition.x - origin.x) / tileSize),
-                                  Mathf.FloorToInt((worldPosition.y - origin.y) / tileSize));
-        }
-
-        public Vector2 GetTileWorldPos(Vector2 tilePos)
-        {
-            return tilePos + origin + new Vector2(tileSize, tileSize) * 0.5f;
-        }
-
-        public void AddTile(Tile spawnTile, TileType tileType)
-        {
-            Vector2Int gridSpawnPosition = GetTileXY(spawnTile.Position);
-
-            spawnTile.Position = GetTileWorldPos(gridSpawnPosition);
-            spawnTile.GridPosition = gridSpawnPosition;
-
-            tiles[gridSpawnPosition.x, gridSpawnPosition.y] = spawnTile;
-
-            BuildTilemap();
-        }
         
-        private void BuildTilemap()
-        {
-            for (int x = 0; x < gridSize.x; x++)
-            {
-                for (int y = 0; y < gridSize.y ; y++) 
-                {
-                    if (tiles[x, y])
-                        tilemap.SetTile(new Vector3Int(x, y, 0) - new Vector3Int(gridSize.x, gridSize.y, 0) / 2, GetTileBase(tiles[x, y].Type));
-                }
-            }
-        }
-
-        private TileBase GetTileBase(TileType type)
-        {
-            if ((int)type < tilemapBaseTiles.Length)
-                return tilemapBaseTiles[(int)type];
-
-            else
-                return tilemapBaseTiles[0];
-        }
-
         private void OnDestroy()
         {
             Instance = null;
